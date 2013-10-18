3ba75f58425741374f63b058c29a182f438b82c3
Caio Pinheiro <pinheiro.caiof@gmail.com>
2013-10-18 11:17:25 -0400

    atualização segunda tela

--- src/br/com/sbseg/activities/TelaKeynoteMainActivity.java
+++ src/br/com/sbseg/activities/TelaKeynoteMainActivity.java
@@ -1,11 +1,16 @@
-package br.com.sbseg.activities;
+package com.keynoteActivity;
 
+import java.util.HashMap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.TextView;
-import br.com.sbseg.R;
-import br.com.sbseg.beans.Palestrante;
+import br.com.beans.Speaker;
+
+import com.example.keynotes.R;
+//import android.widget.ImageView;
 
 public class TelaKeynoteMainActivity extends Activity {
 	
@@ -17,26 +22,53 @@ public class TelaKeynoteMainActivity extends Activity {
 		setContentView(R.layout.activity_tela_keynote_main);
 		
 		Bundle bundle = getIntent().getExtras();
-		Palestrante speaker = (Palestrante) bundle.getSerializable("Speaker");
+		Speaker speaker = (Speaker)bundle.getSerializable("Speaker");
+		String photo = speaker.getPhoto();
 		
 		TextView nomeSpeaker = (TextView)findViewById(R.id.IDNome);
 		TextView filiacaoSpeaker = (TextView)findViewById(R.id.IDFiliacao);
 		TextView tituloSpeaker = (TextView)findViewById(R.id.IDTitulo);
 		TextView resumoSpeaker = (TextView)findViewById(R.id.IDResumo);
 		TextView biografiaSpeaker = (TextView)findViewById(R.id.IDBiografia);
+		ImageView foto = (ImageView)findViewById(R.id.imagem);
+		
+		HashMap<String,Integer> hashMap = new HashMap<String, Integer>();
+		hashMap.put("antonio",R.drawable.antonio);
+    	hashMap.put("davidott",R.drawable.davidott);
+    	hashMap.put("georgecox",R.drawable.georgecox);
+    	hashMap.put("matt",R.drawable.matt);
+    	hashMap.put("fabianmonrose",R.drawable.fabianmonrose);
+    	
+		
+		
+		if(photo==null){
+			foto.setImageResource(R.drawable.ic_launcher);
+		}
+		else{
+			
+			foto.setImageResource(hashMap.get(photo));
+		}
 		
 		nomeSpeaker.setText(speaker.getName());
-	//	nomeSpeaker.canScrollVertically(1);
 		filiacaoSpeaker.setText(speaker.getFiliation());
-//		filiacaoSpeaker.canScrollVertically(1);
-		tituloSpeaker.setText(speaker.getTalk().getTitle());
-//		tituloSpeaker.canScrollVertically(1);
-		resumoSpeaker.setText(speaker.getTalk().getResume());
-	//	resumoSpeaker.canScrollVertically(1);
-		biografiaSpeaker.setText(speaker.getBiography());
-	//	biografiaSpeaker.canScrollVertically(1);
+		
+		tituloSpeaker.setTextSize(12);
+		tituloSpeaker.setText("Titulo: " + speaker.getTalk().getTitle());
+		
+		resumoSpeaker.setTextSize(12);
+		resumoSpeaker.setText("Resumo: " + speaker.getTalk().getResume());
+		
+		biografiaSpeaker.setTextSize(12);
+		biografiaSpeaker.setText("Biografia: " +speaker.getBiography());
+		
 		
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tela_keynote_main, menu);
+		return true;
+	}
 
 }
