e182981bc095b43dc355a80e8b5d021b6db34b0f
Caio Pinheiro <pinheiro.caiof@gmail.com>
2013-10-31 17:10:48 -0400

    Integração com facebook

--- src/br/com/sbseg/activities/SbsegMainActivity.java
+++ src/br/com/sbseg/activities/SbsegMainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import br.com.sbseg.FacebookSbsegMainActivity;
 import br.com.sbseg.R;
 
 public class SbsegMainActivity extends Activity{
@@ -17,6 +18,11 @@ public class SbsegMainActivity extends Activity{
 	
 	public void Calendar(View view){
 		Intent intent = new Intent(this,ProgrammingActivity.class);
+		startActivity(intent);
+	}
+	
+	public void facebook(View view){
+		Intent intent = new Intent(this,FacebookSbsegMainActivity.class);
 		startActivity(intent);
 	}
 
